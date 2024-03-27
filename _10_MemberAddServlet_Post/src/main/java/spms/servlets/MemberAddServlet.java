58db19cbb3aade1dc9caaa871c40d88c9bf96a1b
서혜리 <hye_dam_2@naver.com>
2024-03-27 17:13:22 +0900

    Statement와 PreparedStatement의 차이를 주석으로 설명 추가

--- _10_MemberAddServlet_Post/src/main/java/spms/servlets/MemberAddServlet.java
+++ _10_MemberAddServlet_Post/src/main/java/spms/servlets/MemberAddServlet.java
@@ -42,6 +42,19 @@ public class MemberAddServlet extends HttpServlet {
 		// 이 설정을 안 해주면 한글이 깨짐
 		req.setCharacterEncoding("UTF-8");
 		Connection conn = null;
+		
+		/**
+		 * Statement
+		 * 	- 질의할 때마다 sql을 컴파일한다.
+		 * 	- 입력 매개변수가 여러 개 필요할 때 문자열 결합연산자인 +를 이용해서 해야 한다. (불편)
+		 * 	- 전송 직전에 sql문을 입력받고, 컴파일 한 후 서버로 전송한다.
+		 * 
+		 * PreparedStatement
+		 * 	- sql문을 미리 입력하여 컴파일한 상태에서 객체를 받는다.
+		 * 	- 만약에 sql문 구조가 변경되지 않고, 파라미터값만 바뀌는 경우 Statement보다 훨씬 빠르다.
+		 * 	- 입력 매개변수가 여러 개 필요할 때 ?로 sql문의 파라미터를 표시하고, 나중에 전달하므로 편하다.
+		 * 	- Statement < PreparedStatement를 사용한다.
+		 */
 		PreparedStatement pstmt = null;
 		
 		try {
