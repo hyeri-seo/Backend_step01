9aee2aee5c029e929cbb1df133b2dfb4fbcd9f7e
서혜리 <hye_dam_2@naver.com>
2024-04-01 12:42:52 +0900

    MemberUpdateServlet 수정

--- _38_Question3/src/main/java/spms/servlets/MemberUpdateServlet.java
+++ _38_Question3/src/main/java/spms/servlets/MemberUpdateServlet.java
@@ -30,42 +30,20 @@ public class MemberUpdateServlet extends HttpServlet{
 		Statement stmt = null;
 		ResultSet rs = null;
 		try {
-//			ServletContext sc = this.getServletContext();
-//			Class.forName(sc.getInitParameter("driver"));
-//			conn = DriverManager.getConnection(
-//						sc.getInitParameter("url"),
-//						sc.getInitParameter("username"),
-//						sc.getInitParameter("password")
-//					);
-//			stmt = conn.createStatement();
-//			rs = stmt.executeQuery(
-//					"SELECT mno,email,mname,cre_date FROM members" +
-//					" WHERE mno=" + req.getParameter("no")
-//					);
-//			
-//			if (rs.next()) {
-//				req.setAttribute("member", 
-//					new Member()
-//						.setNo(rs.getInt("MNO"))
-//						.setEmail(rs.getString("EMAIL"))
-//						.setName(rs.getString("MNAME"))
-//						.setCreatedDate(rs.getDate("CRE_DATE")));
-//			} else {
-//				throw new Exception("해당 번호의 회원을 찾을 수 없습니다.");
-//			}
+			ServletContext sc = this.getServletContext();
+			conn = (Connection) sc.getAttribute("conn");
 			
-			
-			// 추가 ----------------------------------------
 			MemberDao memberDao = new MemberDao();
-			memberDao.selectOne(Integer.parseInt(req.getParameter("no")));
-			// ----------------------------------------
+			memberDao.setConnection(conn);
+			Member member = memberDao.selectOne(Integer.parseInt(req.getParameter("no")));
+			
+			req.setAttribute("member", member);
 			
 			RequestDispatcher rd = req.getRequestDispatcher(
 					"/member/MemberUpdateForm.jsp");
 			rd.forward(req, resp);
 			
 		}catch(Exception e) {
-			//throw new ServletException(e);
 			e.printStackTrace();
 			req.setAttribute("error", e);
 			RequestDispatcher rd = req.getRequestDispatcher("/Error.jsp");
@@ -73,7 +51,6 @@ public class MemberUpdateServlet extends HttpServlet{
 		}finally {
 			try {if(rs!=null) rs.close();} catch(Exception e) {}
 			try {if(stmt!=null) stmt.close();} catch(Exception e) {}
-			try {if(conn!=null) conn.close();} catch(Exception e) {}			
 		}
 	}
 
@@ -81,37 +58,18 @@ public class MemberUpdateServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("MemberUpdateServlet::doPost() 호출");
 		
-		// CharacterEncodingFilter로 전처리 했으므로 이제 안해도 됨
-		//req.setCharacterEncoding("UTF-8");
-		
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		try {
 			ServletContext sc = this.getServletContext();
 			Class.forName(sc.getInitParameter("driver"));
-			conn = DriverManager.getConnection(
-						sc.getInitParameter("url"),
-						sc.getInitParameter("username"),
-						sc.getInitParameter("password")
-					);
-			stmt = conn.prepareStatement(
-						"UPDATE members SET email=?, mname=?, mod_date=NOW()"
-					+ " WHERE mno=?"
-					);
-			stmt.setString(1,  req.getParameter("email"));
-			stmt.setString(2,  req.getParameter("name"));
-			stmt.setInt(3,  Integer.parseInt(req.getParameter("no")));
-			stmt.executeUpdate();
-			
-			// 추가 ----------------------------------------
-			Member member = new Member();
-			member.setEmail(req.getParameter("email"));
-			member.setEmail(req.getParameter("name"));
-			member.setNo(Integer.parseInt(req.getParameter("no")));
+			conn = (Connection) sc.getAttribute("conn");
 			
 			MemberDao memberDao = new MemberDao();
-			memberDao.update(member);
-			// ----------------------------------------
+			memberDao.update(new Member()
+							.setEmail(req.getParameter("email"))
+							.setName(req.getParameter("name"))
+							.setNo(Integer.parseInt(req.getParameter("no"))));
 			
 			resp.sendRedirect("list");
 			
@@ -123,7 +81,6 @@ public class MemberUpdateServlet extends HttpServlet{
 			rd.forward(req, resp);
 		}finally {
 			try {if(stmt!=null) stmt.close();} catch(Exception e) {}
-			try {if(conn!=null) conn.close();} catch(Exception e) {}
 		}
 	}
 }
