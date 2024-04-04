a38bd7be023b32d39312f8cd7537ee4c0a8ae456
서혜리 <hye_dam_2@naver.com>
2024-04-04 11:43:58 +0900

    _58_Application_Context_File

--- _58_Application_Context_File/src/main/java/spms/listeners/ContextLoaderListener.java
+++ _58_Application_Context_File/src/main/java/spms/listeners/ContextLoaderListener.java
@@ -1,23 +1,30 @@
 package spms.listeners;
 
-import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.sql.DataSource;
+import javax.servlet.annotation.WebListener;
 
-import spms.controls.LogInController;
-import spms.controls.LogOutController;
-import spms.controls.MemberAddController;
-import spms.controls.MemberDeleteController;
-import spms.controls.MemberListController;
-import spms.controls.MemberUpdateController;
-import spms.dao.MemberDao;
-import spms.dao.MySqlMemberDao;
+import spms.context.ApplicationContext;
 
 
-//@WebListener
+@WebListener
 public class ContextLoaderListener implements ServletContextListener{
+	
+	/* ContextLoaderListener는 WebApp이 시작될 때 이벤트 통보를 받는 역할이므로
+	 * 이후에는 사용하지 않음
+	 * 그러므로 객체를 별도로 접근하지 않기 때문에
+	 * 객체를 통해 메서드를 호출하지 않고
+	 * 클래스를 통해서 어디에서나 호출할 수 있도록
+	 * static을 사용함
+	 * 아래처럼 접근해서 사용하려는 의도
+	 * ApplicationContext appContext = ContextLoaderListener.getApplicationContext();
+	 * */
+	static ApplicationContext applicationContext;
+	
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
 	
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
@@ -26,7 +33,11 @@ public class ContextLoaderListener implements ServletContextListener{
 		try {
 			ServletContext sc = sce.getServletContext();
 			
-
+			String propertiesPath = sc.getRealPath(
+							sc.getInitParameter("contextConfigLocation"));
+			applicationContext = new ApplicationContext(propertiesPath);
+			
+			/*
 			InitialContext initialContext = new InitialContext();
 			DataSource ds = (DataSource)initialContext.lookup(
 						"java:comp/env/jdbc/studydb");
@@ -40,8 +51,7 @@ public class ContextLoaderListener implements ServletContextListener{
 			sc.setAttribute("/member/add.do", new MemberAddController().setMemberDao(memberDao));
 			sc.setAttribute("/member/update.do", new MemberUpdateController().setMemberDao(memberDao));
 			sc.setAttribute("/member/delete.do", new MemberDeleteController().setMemberDao(memberDao));
-			
-			//sc.setAttribute("memberDao", memberDao);
+			*/
 			
 		}catch(Exception e) {
 			e.printStackTrace();
