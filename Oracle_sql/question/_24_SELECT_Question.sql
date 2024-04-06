6e1e385b4e4a7dd53ffcee3167e11b3fdfd4d2bf
서혜리 <hye_dam_2@naver.com>
2024-04-07 00:17:15 +0900

    단일행함수

--- Oracle_sql/question/_24_SELECT_Question.sql
+++ Oracle_sql/question/_24_SELECT_Question.sql
@@ -18,12 +18,13 @@ FROM professor;
 
 4) 일반 과목을 기초 과목으로 변경해서 모든 과목을 검색하세요
    (ex. 일반화학 -> 기초화학)
-SELECT REPLACE(cname, '일반', '기초')v
+SELECT REPLACE(cname, '일반', '기초') 
 FROM course 
 WHERE cname LIKE '일반%';
    
 5) 만일 입력 실수로 student테이블의 sname컬럼에 데이터가 입력될 때
    문자열 마지막에 공백이 추가되었다면 검색할 때 이를 제외하고
    검색하는 SELECT 문을 작성하세요
-SELECT 
+SELECT TRIM(trailing ' ' FROM sname) 
+FROM student;
    
